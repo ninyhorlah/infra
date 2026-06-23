94e08ed17706da9310aa55dcd3581f097d605fb5
ninyhorlah <ninyhorlah6@gmail.com>
2026-06-23 18:07:09 +0200

    update

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ module "ecr_repo" {
   source = "./modules/ecr"
   ecr_repository_name = var.ecr_repository_name
 }
+
+module iam_role {
+  source = "./modules/iam_role"
+  ecr_repository_policy_name = "clarity-ecr-repo-policy"
+}
---
+++ modules/iam_role/variables.tf
@@ -0,0 +1,4 @@
+variable "ecr_repository_policy_name" {
+  description = "The name of the ECR repository"
+  type        = string 
+}
