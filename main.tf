12ba9dd1cbfe1961b8eea23d3c8cc1e9df28aa75
ninyhorlah <ninyhorlah6@gmail.com>
2026-06-25 14:46:50 +0200

    update

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ data "aws_ecr_repositories" "ecr_repositories" {}
 
 module "ecr_repo" {
   source = "./modules/ecr"
-  ecr_repository_name = [for repo in data.aws_ecr_repositories.ecr_repositories.names : repo if repo != var.ecr_repository_name]
+  ecr_repository_name = [for repo in data.aws_ecr_repositories.ecr_repositories.names : var.ecr_repository_name if repo != var.ecr_repository_name]
 }
