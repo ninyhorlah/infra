cfe199b8ca0510275fd85adea9cb3951b76e450b
ninyhorlah <ninyhorlah6@gmail.com>
2026-06-26 14:13:49 +0200

    update role

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -39,7 +39,7 @@ resource "aws_iam_role" "ecr_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
+        Action = ["sts:AssumeRole", "sts:TagSession"]
         Effect = "Allow"
         Sid    = ""
         Principal = {
