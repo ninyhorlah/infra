f19044716b85f8cd7e64a895911df892a19aaf51
ninyhorlah <ninyhorlah6@gmail.com>
2026-06-26 13:11:14 +0200

    update role

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -4,35 +4,46 @@ data "aws_region" "current" {}
 
 data "aws_partition" "current" {}
 
-resource "aws_iam_role" "test_role" {
-  name = var.ecr_iam_role
+resource "aws_iam_role_policy" "ecr_role_policy" {
+  name = "ecr_role_policy"
+  role = aws_iam_role.ecr_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "ecr:GetAuthorizationToken"
-        Effect = "Allow"
-        Sid    = "GetAuthorizationToken"
-        Principal = {
-          Service = "ecr.amazonaws.com"
-        }
-      },
-      {
-        Action = [
+  policy = jsonencode({
+    "Version":"2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
                 "ecr:CompleteLayerUpload",
                 "ecr:UploadLayerPart",
                 "ecr:InitiateLayerUpload",
                 "ecr:BatchCheckLayerAvailability",
                 "ecr:PutImage",
                 "ecr:BatchGetImage"
-            ]
+            ],
+            "Resource": "${data.aws_partition.current.partition}:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:repository/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ecr:GetAuthorizationToken",
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+resource "aws_iam_role" "ecr_role" {
+  name = var.ecr_iam_role
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
         Effect = "Allow"
         Sid    = ""
         Principal = {
-            Service = "ecr.amazonaws.com"
+          Service = "ecr.amazonaws.com"
         }
       },
     ]
